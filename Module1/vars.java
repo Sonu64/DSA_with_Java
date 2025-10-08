bcfa57c4bf3509b28de35c59068c82196a765fc3
Sonu64 <sonusantu64@gmail.com>
2025-10-08 22:59:05 +0530

    Initial commit

---
+++ Module1/vars.java
@@ -0,0 +1,7 @@
+public class vars {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Sourakanti";
+        System.out.println("Hello I am " + name + " and I am " + age + " years old !");
+    }
+}

a9d7a3572d86a6628659141bc192a940e6589ce6
Sonu64 <sonusantu64@gmail.com>
2025-10-10 20:28:03 +0530

    Update continueKeyword.java

--- Module3_Loops/continueKeyword.java
+++ Module3_Loops/continueKeyword.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class continueKeyword {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num;
+        System.out.println("Keep on entering numbers, program will skip displaying multiples of 10.");
+        System.out.println("Enter -1 to exit program.");
+        while(true) {
+            System.out.print("Enter number: ");
+            num = in.nextInt();
+            if (num % 10 == 0)
+                continue;
+            if (num == -1) {
+                System.out.println("-1 entered, exiting program ...");
+                break;
+            }
+            System.out.println(num);
+        }
+    }
 }

0b0b972d0e0401bd875aa460a3d55f8ad53d98f0
Sonu64 <sonusantu64@gmail.com>
2025-10-10 20:37:16 +0530

    Create prime.java

---
+++ Module3_Loops/prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num, i;
+        boolean isPrime = true;
+
+        System.out.print("Enter a number to check for Prime: ");
+        num = in.nextInt();
+
+        for(i = 2; i < num/2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a Prime number !");
+        }
+        else {
+            System.out.println(num + " is not a Prime number !");
+        }
+    }
+}

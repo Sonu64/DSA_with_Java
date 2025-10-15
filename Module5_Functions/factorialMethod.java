248637bc859ffc03198c58c8474544a6c3e70a84
Sonu64 <sonusantu64@gmail.com>
2025-10-15 21:16:32 +0530

    Update factorialMethod.java

--- Module5_Functions/factorialMethod.java
+++ Module5_Functions/factorialMethod.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class factorialMethod {
+
+    public static int findFactorial(int num) {
+        int factorial = 1;
+        while (num != 0) {
+            factorial *= num;
+            num = num - 1;
+        }
+        return factorial;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number to find factorial: ");
-        
+        int num = in.nextInt();
+        System.out.printf("Factorial of %d is %d.\n", num, findFactorial(num));
     }
 }

a572944c275958cbbd1825e0bc2bcaae53e30876
Sonu64 <sonusantu64@gmail.com>
2025-10-20 14:42:52 +0530

    Update maxSubArraySum.java

--- Module6_Arrays1/maxSubArraySum.java
+++ Module6_Arrays1/maxSubArraySum.java
@@ -6,7 +6,6 @@ public class maxSubArraySum {
     public static int findMaxSubArraySum(int nums[], int SIZE) {
         int max = Integer.MIN_VALUE, sum = 0;
         for (int i = 0; i < SIZE; i++) {
-            sum = max;
             for (int j = i; j < SIZE; j++) {
                 sum = 0;
                 for (int k = i; k < j + 1; k++) {
@@ -22,7 +21,7 @@ public class maxSubArraySum {
     }
 
     public static void main(String[] args) {
-        int nums[] = {2, 4, 6, -1, -2, 5};
+        int nums[] = {2, 4, 6};
         System.out.println("Maximum Subarray Sum is " + findMaxSubArraySum(nums, nums.length) + ". ");
     }
 }

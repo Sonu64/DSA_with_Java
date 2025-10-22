620fb550ccd96bbb6f08ac0ded95bb78d534554d
Sonu64 <sonusantu64@gmail.com>
2025-10-22 20:25:11 +0530

    Update maxSubArraySumKadanesAlgorithm.java

--- Module6_Arrays1/maxSubArraySumKadanesAlgorithm.java
+++ Module6_Arrays1/maxSubArraySumKadanesAlgorithm.java
@@ -1,22 +1,29 @@
 public class maxSubArraySumKadanesAlgorithm {
-
     public static int applyKadanesAlgorithm(int nums[], int SIZE) {
-        int sum = 0, max = Integer.MIN_VALUE;
-        for(int i = 0; i < SIZE; i++) {
+        int sum = 0, max = Integer.MIN_VALUE, startingPosition = 0, endingPosition = 0, i;
+        for(i = 0; i < SIZE; i++) {
             //sum = 0
             sum = sum + nums[i];
-
-            if (sum > max)
+            if (sum > max) {
                 max = sum;
-
-            if (sum < 0)
+                endingPosition = i;
+            }
+            if (sum < 0) {
                 sum = 0;
+                startingPosition++;
+            }
         }
+        
+        //maxPivot = i;
+        System.out.println("Subarray with Maximum Sum is - ");
+        for(int j = startingPosition; j < endingPosition + 1; j++) {
+            System.out.print(nums[j] + " ");
+        }
+        System.out.println();
         return max;
     }
-
     public static void main(String[] args) {
-        int nums[] = {2, 4, 6, 8, -1, -2, 20};
+        int nums[] = {2, 4, 6, 8};
         System.out.println("Maximum Subarray Sum is " + applyKadanesAlgorithm(nums, nums.length) + ". ");
     }
 }

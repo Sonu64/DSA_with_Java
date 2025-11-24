fefb76de23f3124e646546ef887a660d3dd3c2ee
Sonu64 <sonusantu64@gmail.com>
2025-11-24 11:40:11 +0530

    changed approach of spiralMatrix and solved edge Cases

--- Module11_2D_Arrays/spiralMatrix.java
+++ Module11_2D_Arrays/spiralMatrix.java
@@ -8,34 +8,54 @@ public class spiralMatrix {
             for (int j = startingCol; j <= endingCol; j++) {
                 System.out.print(arr[startingRow][j] + "  ");
             }
+            startingRow++;
             // Print RIGHT, startingRow+1 as 1st element of starting row already printed
-            for (int i = startingRow + 1; i <= endingRow; i++) {
+            for (int i = startingRow; i <= endingRow; i++) {
                 System.out.print(arr[i][endingCol] + "  ");
             }
+            endingCol--;
             // Print BUTTOM, endingCol-1 as RIGHT printing already printed arr[endingRow][endingCol]
-            for (int j = endingCol - 1; j >= startingCol; j--) {
-                System.out.print(arr[endingRow][j] + "  ");
+            // For Single Row, don't print buttom
+            if (startingRow <= endingRow) {
+                for (int j = endingCol; j >= startingCol; j--) {
+                    System.out.print(arr[endingRow][j] + "  ");
+                }
+                endingRow--;
             }
             // Print LEFT, endingRow-1 as BUTTOM printed arr[endingRow][startingCol]
             // startingRow+1 as the 1st element already printed by TOP
-            for (int i = endingRow - 1; i >= startingRow + 1; i--) {
-                System.out.print(arr[i][startingCol] + "  ");
+            if (startingCol <= endingCol) {
+                for (int i = endingRow; i >= startingRow; i--) {
+                    System.out.print(arr[i][startingCol] + "  ");
+                }
+                startingCol++;
             }
-
-            startingRow++;
-            endingRow--;
-            startingCol++;
-            endingCol--;
         }
     }
 
     public static void main (String[] args) {
         int arr[][] = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
+                {1},
+                {2},
+                {3}
         };
         printSpiral(arr, arr.length, arr[0].length);
+        System.out.println("\n\n");
+
+
+        int arr2[][] = {
+                {1, 2, 3}
+        };
+        printSpiral(arr2, arr2.length, arr2[0].length);
+        System.out.println("\n\n");
+
+
+        int arr3[][] = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        printSpiral(arr3, arr3.length, arr3[0].length);
+        System.out.println("\n\n");
     }
 }

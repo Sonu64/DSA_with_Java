d3d5a64b2a5729e9b0c0683c80dc36f8846678a7
Sonu64 <sonusantu64@gmail.com>
2025-11-25 16:35:13 +0530

    Added optimized Diagonal Sum Function and stairCase Search in 2D Arrays.

--- Module11_2D_Arrays/diagonalSum.java
+++ Module11_2D_Arrays/diagonalSum.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class diagonalSum {
 
-    public static int primaryDiagonalSum(int arr[][], int rows, int cols) {
+    public static int primaryDiagonalSum(int arr[][], int n) {
         int sum = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
                 if (i == j)
                     sum += arr[i][j];
         }
         return sum;
     }
 
-    public static int secondaryDiagonalSum(int arr[][], int rows, int cols) {
+    public static int secondaryDiagonalSum(int arr[][], int n) {
         int sum = 0;
-        int i = 0, j = cols - 1;
-        while (i < rows && j >= 0) {
+        int i = 0, j = n - 1;
+        while (i < n && j >= 0) {
             if (i != j) {
                 sum += arr[i][j];
             }
@@ -25,8 +25,17 @@ public class diagonalSum {
         return sum;
     }
 
-    public static int optimizedDiagonalSum(int arr[][], int rows, int cols) {
-        return 0;
+    public static int optimizedDiagonalSum(int arr[][], int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            // Primary Diagonal Sum
+            sum += arr[i][i];
+            // Secondary Diagonal Sum
+            int j = n - 1 - i;
+            if (i != j)
+                sum += arr[i][j];
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -43,9 +52,9 @@ public class diagonalSum {
             for(int j=0; j < matrix1[0].length; j++)
                 matrix1[i][j] = in.nextInt();
         }
-        primaryDiagonalSum1 = primaryDiagonalSum(matrix1, matrix1.length, matrix1[0].length);
+        primaryDiagonalSum1 = primaryDiagonalSum(matrix1, matrix1.length);
         System.out.println("Primary Diagonal Sum = " + primaryDiagonalSum1);
-        secondaryDiagonalSum1 = secondaryDiagonalSum(matrix1, matrix1.length, matrix1[0].length);
+        secondaryDiagonalSum1 = secondaryDiagonalSum(matrix1, matrix1.length);
         System.out.println("Secondary Diagonal Sum = " + secondaryDiagonalSum1);
         diagonalSums1 = primaryDiagonalSum1 + secondaryDiagonalSum1;
         System.out.println("Diagonal Sum = " + diagonalSums1);
@@ -57,11 +66,15 @@ public class diagonalSum {
             for(int j=0; j < matrix2[0].length; j++)
                 matrix2[i][j] = in.nextInt();
         }
-        primaryDiagonalSum2 = primaryDiagonalSum(matrix2, matrix2.length, matrix2[0].length);
+        primaryDiagonalSum2 = primaryDiagonalSum(matrix2, matrix2.length);
         System.out.println("Primary Diagonal Sum = " + primaryDiagonalSum2);
-        secondaryDiagonalSum2 = secondaryDiagonalSum(matrix2, matrix2.length, matrix2[0].length);
+        secondaryDiagonalSum2 = secondaryDiagonalSum(matrix2, matrix2.length);
         System.out.println("Secondary Diagonal Sum = " + secondaryDiagonalSum2);
         diagonalSums2 = primaryDiagonalSum2 + secondaryDiagonalSum2;
         System.out.println("Diagonal Sum = " + diagonalSums2);
+
+        System.out.println("\nUsing Optimized Approach - ");
+        System.out.println("Diagonal Sum of 1st Matrix = " + optimizedDiagonalSum(matrix1, matrix1.length));
+        System.out.println("Diagonal Sum of 2nd Matrix = " + optimizedDiagonalSum(matrix2, matrix2.length));
     }
 }
---
+++ Module11_2D_Arrays/searchInSortedMatrix.java
@@ -0,0 +1,31 @@
+public class searchInSortedMatrix {
+
+    private static void stairCaseSearch(int key, int[][] arr, int rows, int cols) {
+        int rowIndex = 0, colIndex = cols-1;
+        while (rowIndex < rows && colIndex < cols) {
+            if (key < arr[rowIndex][colIndex]) {
+                // Move LEFT
+                colIndex--;
+            }
+            else if (key > arr[rowIndex][colIndex]){
+                // Move DOWN
+                rowIndex++;
+            }
+            else {
+                System.out.println(key + " is found at Position " + rowIndex + ", " + colIndex + ".");
+                return;
+            }
+        }
+        System.out.println(key + " is not found in the matrix.");
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {27, 29, 37, 48},
+                {32, 33, 39, 50}
+        };
+        stairCaseSearch(35, arr, arr.length, arr[0].length);
+    }
+}

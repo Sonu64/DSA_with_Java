78946b574e88c29461d3ec770190a5bf6b9c64e0
Sonu64 <sonusantu64@gmail.com>
2025-11-26 09:15:38 +0530

    Update homework1And2.java

--- Module11_2D_Arrays/homework1And2.java
+++ Module11_2D_Arrays/homework1And2.java
@@ -8,6 +8,14 @@ public class homework1And2 {
         return count;
     }
 
+
+    private static int sumOf2ndRow(int arr[][], int rows, int cols) {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+            sum += arr[1][j];
+        return sum;
+    }
+
     public static void main (String[] args) {
         int arr[][] = {
                 {8, 8, 9, 10},
@@ -15,5 +23,6 @@ public class homework1And2 {
                 {6, 7, 8, 9}
         };
         System.out.println("7 occurs " + countOccurences(7, arr, arr.length, arr[0].length) + " times.");
+        System.out.println("Sum of 2nd Row of the Matrix is " + sumOf2ndRow(arr, arr.length, arr[0].length) + ".");
     }
 }

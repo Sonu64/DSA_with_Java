6acd7f5d27c8003ac604ca276a17ab8929b141e9
Sonu64 <sonusantu64@gmail.com>
2025-11-29 13:04:27 +0530

    Added String Compression Program : Asked in Amazon -> coded by myself totally

---
+++ Module12_Strings/compression.java
@@ -0,0 +1,44 @@
+public class compression {
+
+    public static String findCompressedString(String s, int n) {
+        Integer count = 0;
+        Character lastChar = s.charAt(0);
+        StringBuilder c = new StringBuilder("");
+        for (int i = 0; i < n; i++) {
+            // New Character found and it is NOT Last character of the String.
+            if (s.charAt(i) != lastChar && i != n - 1) {
+                c.append(lastChar.toString());
+                if (count != 1)
+                    c.append(count.toString());
+                count = 1;
+            }
+            // Last character reached and is different from previous characters -> Mulltiple Occurence
+            // of Last character 
+            else if (i == n - 1 && s.charAt(i) == lastChar) {
+                c.append(lastChar.toString());
+                count++;
+                if (count != 1)
+                    c.append(count.toString());
+            } 
+            // Last character reached and is different from previous characters -> Single Occurence
+            // of Last character.
+            else if (i == n - 1 && s.charAt(i) != lastChar) {
+                c.append(lastChar.toString());
+                if (count != 1)
+                    c.append(count.toString());
+                c.append(s.charAt(i));
+            }
+            // New Character not found, just increase count of old lastChar, Last character NOT reached 
+            else {
+                count++;
+            }
+            lastChar = s.charAt(i);
+        }
+        String compressedString = c.toString();
+        return compressedString;
+    }
+    public static void main(String[] args) {
+        String longString = "aabbbcdd";
+        System.out.println(findCompressedString(longString, longString.length()));
+    }
+}

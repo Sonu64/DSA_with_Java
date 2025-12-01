f47a345af1cbcc916a9d36be9a2159068d60577e
Sonu64 <sonusantu64@gmail.com>
2025-12-01 15:41:30 +0530

    Started Bit Manipulation and added bitwiseOperators.java

---
+++ Module13_Bit_Manipulation/bitwiseOperators.java
@@ -0,0 +1,9 @@
+public class bitwiseOperators {
+    public static void main(String[] args) {
+        System.out.println("Bitwise AND - 5 & 6 = " + (5 & 6));
+        System.out.println("Bitwise OR - 5 | 6 = " + (5 | 6));
+        System.out.println("Bitwise NOT (Binary 1's complement) of 5, ~5 = " + (~5));
+        System.out.println("Left Shifting 5 by 2 bits, 5 << 2 = " + (5 << 2));
+        System.out.println("Right Shifting 5 by 2 bits, 5 >> 2 = " + (5 >> 2));
+    }
+}

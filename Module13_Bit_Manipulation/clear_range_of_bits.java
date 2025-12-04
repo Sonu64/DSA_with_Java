a092437e3139484002c94cc23a5e3bd5128815e2
Sonu64 <sonusantu64@gmail.com>
2025-12-04 11:23:07 +0530

    Create clear_range_of_bits.java

---
+++ Module13_Bit_Manipulation/clear_range_of_bits.java
@@ -0,0 +1,8 @@
+public class clear_range_of_bits {
+
+    public static int clearRangeOfBits(int num, int i, int j) {
+        int totalBits = (j-i)+1;
+        // We have to clear last totalBits from position i
+
+    }
+}

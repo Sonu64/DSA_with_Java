e726a5cdc3a24a9aeaa023c2124164763c2ad438
Sonu64 <sonusantu64@gmail.com>
2025-12-09 21:50:25 +0530

    Update singleNumber2.java
    Solved in Leetcode and understood the Logic why we were doing bitCount << bitPosition instead of 1 << bitPosition. Later fixed this behavious using conditionals and used result = result ^ (1 << bitPosition)

--- Module13_Bit_Manipulation/singleNumber2.java
+++ Module13_Bit_Manipulation/singleNumber2.java
@@ -1,21 +1,18 @@
 public class singleNumber2 {
-    public static int findUnique(int[] nums) {
-        int result = 0;
-        // Loop through 32 positions, and for each position Loop over all numbers
-        for(int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            int bitCount = 0;
-            for (int number : nums) {
-                // Get i-th bit of number where i = bitPosition
-                int bit = number & (1 << bitPosition);
-                if (bit != 0) bitCount++;
-            }
-            bitCount = bitCount % 3; // Will either be 0 or 1
-            System.out.println(bitCount);
-            // Set bit at bitPosition to 1 in result
-            result = result | (bitCount << bitPosition);
-        }
+    public static int findUnique(int[] arr) {
 
-        return result;
+            int result = 0;
+            for(int bitPosition=0; bitPosition<32; bitPosition++) {
+                int bitCount = 0;
+                for(int num : arr) {
+                    if ((num & (1 << bitPosition)) != 0)
+                        bitCount++;
+                }
+                bitCount %= 3;
+                if (bitCount == 1)
+                    result = result ^ (1 << bitPosition);
+            }
+            return result;
     }
 
     public static void main(String[] args) {

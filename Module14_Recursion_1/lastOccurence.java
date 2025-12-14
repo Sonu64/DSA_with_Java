1103047f79f5195f5b2f5432e4bc48407222a485
Sonu64 <sonusantu64@gmail.com>
2025-12-14 16:19:55 +0530

    added code if target is not found in array, for both firstOccurence.java and lastOccurence.java

--- Module14_Recursion_1/lastOccurence.java
+++ Module14_Recursion_1/lastOccurence.java
@@ -1,7 +1,8 @@
 public class lastOccurence {
     public static int find_last_occurence(int[] arr, int target, int n) {
         int i = n-1;
-        int index = 0;
+        if (i == 0 && arr[i] != target)
+            return -1;
         if (arr[i] == target)
             return i;
         else
@@ -9,7 +10,7 @@ public class lastOccurence {
     }
 
     public static void main(String[] args) {
-        int arr[] = {2, 4, 6, 7, 6, 7, 6};
-        System.out.println(find_last_occurence(arr, 6, arr.length));
+        int arr[] = {2, 4, 6, 7, 6, 7, 6, 90};
+        System.out.println(find_last_occurence(arr, 90, arr.length));
     }
 }

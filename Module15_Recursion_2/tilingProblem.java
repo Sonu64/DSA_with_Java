ebcf57f6d3f6b5bdba362b19d055ac8e646565ea
Sonu64 <sonusantu64@gmail.com>
2025-12-17 16:38:50 +0530

    Started 2nd Recursion Module, added Tiling Problem Program (tilingProblem.java)

---
+++ Module15_Recursion_2/tilingProblem.java
@@ -0,0 +1,17 @@
+public class tilingProblem {
+    public static int numberOfWays(int n) {
+        // code here
+        if (n==0 || n==1) return 1;
+
+        // Choice of Horizontal
+        int horizontalWays = numberOfWays(n-1);
+        // Choice of Vertical
+        int verticalWays = numberOfWays(n-2);
+
+        return (horizontalWays + verticalWays);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfWays(4));
+    }
+};

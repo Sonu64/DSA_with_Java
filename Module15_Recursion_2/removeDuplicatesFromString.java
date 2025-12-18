ba1e12a484bd312bbfad2d2afe67c18bbfc602f5
Sonu64 <sonusantu64@gmail.com>
2025-12-18 13:23:58 +0530

    Fixed removeDuplicatesFromString.java using Recursion --> Self solved taking the hint of Array holding boolean values for a letter occured or not.

--- Module15_Recursion_2/removeDuplicatesFromString.java
+++ Module15_Recursion_2/removeDuplicatesFromString.java
@@ -1,28 +1,32 @@
 public class removeDuplicatesFromString {
-    public static StringBuilder removeDuplicates(StringBuilder s, int n) {
+    public static StringBuilder removeDuplicates(StringBuilder s, int n, boolean[] letters) {
         StringBuilder uniqueString = new StringBuilder("");
-        int index = n - 1;
-        char currentChar = s.charAt(index);
-
-        // Base case for Single Character String
-        if (index == 0) {
-            uniqueString.append(s.charAt(index));
+        int index = s.length() - n;
+        Character currentChar = s.charAt(index);
+        if (index == s.length() - 1) {
+            if ( ! letters[currentChar - 97]) {
+                uniqueString.append(currentChar);
+            }
             return uniqueString;
         }
 
-        // Global Case
-//        for (int i = 0; i <= index; i++) {
-//            if (s.charAt(i) == currentChar) {
-//                s.delete(i, i+1);
-//            }
-//        }
-       return uniqueString.append(uniqueString.append(removeDuplicates(s, n-1)));
-
+        else {
+            if ( ! letters[currentChar - 97]) {
+                uniqueString.append(currentChar);
+                letters[currentChar - 97] = true;
+            }
+            uniqueString.append( removeDuplicates(s, n-1, letters) );
+            return uniqueString;
+        }
     }
 
     public static void main(String[] args) {
-        StringBuilder myString = new StringBuilder("ssoonunuu");
-        myString = removeDuplicates(myString, myString.length());
+        StringBuilder myString = new StringBuilder("abssabbccddczzxxmooppfrfr");
+        boolean letters[] = new boolean[26];
+        for(int i=0; i<26; i++) {
+            letters[i] = false;
+        }
+        myString = removeDuplicates(myString, myString.length(), letters);
         System.out.println(myString);
     }
 }

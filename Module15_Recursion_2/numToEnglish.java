2227c372e741dc6f8566585bdee9ff895c9d36bd
Sonu64 <sonusantu64@gmail.com>
2025-12-24 15:17:33 +0530

    Refactor numToEnglish to improve recursion logic
    Simplified the toEnglish method by handling single-digit and multi-digit cases more clearly. Updated prompt message to clarify that leading zeroes are ignored. Removed redundant code and improved string formatting for output.

--- Module15_Recursion_2/numToEnglish.java
+++ Module15_Recursion_2/numToEnglish.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
 
 public class numToEnglish {
-
     public static StringBuilder toEnglish(int num, StringBuilder result) {
-        String words[] = {"Zero ", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine "};
+        String words[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
 //        System.out.println(result);
-        if (num == 0 && result.isEmpty()) {
-            return new StringBuilder("Zero ");
+        if (num < 10) {
+            result.append(words[num]);
         }
-        else if (num == 0 && !result.isEmpty()) {
-            result.append("Zero ");
+        else {
+            int lastDigit = num % 10;
+            toEnglish(num / 10, result);
+            result.append(" " +  words[lastDigit]);
         }
-        int lastDigit = num % 10;
-        result.append(words[lastDigit]);
-        result = toEnglish(num / 10, result);
         return result;
-        }
+    }
 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Number: (no Zeroes at end): ");
+        System.out.print("Enter Number: (Beginning Zeroes will be ignored): ");
         int num = in.nextInt();
         System.out.print("English representation of " + num + " is ");
         StringBuilder result =  toEnglish(num, new StringBuilder(""));

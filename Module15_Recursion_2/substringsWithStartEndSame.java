e348cd3bba6c20aa4b021ecc625b3eda1c557534
Sonu64 <sonusantu64@gmail.com>
2025-12-27 20:06:23 +0530

    Add program to count substrings with same start and end
    Introduces substringsWithStartEndSame.java, which recursively counts the number of substrings in a string where the first and last characters are the same. Includes a main method for user input and output.

---
+++ Module15_Recursion_2/substringsWithStartEndSame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class substringsWithStartEndSame {
+    static int findSpecialSubstrings(String s, int start, int end) {
+        System.out.println("f(" + start + ", " + end + ")");
+        int count = 0;
+        // Base Cases when length of String is 1 or 0
+        if (end-start == 1 || end==start) {
+            return end-start;
+        }
+        // Fallback Case
+        if (s.charAt(start) == s.charAt(end-1))
+            count++;
+        count = count + findSpecialSubstrings(s, start+1, end) + findSpecialSubstrings(s, start, end-1) - findSpecialSubstrings(s, start+1, end-1);
+        return count;
+    }
+
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s = in.nextLine();
+        int count = findSpecialSubstrings(s, 0, s.length());
+        System.out.println("Number of req. substrings = " + count + ".");
+    }
+}

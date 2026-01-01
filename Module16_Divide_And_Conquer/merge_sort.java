f3e07f2d18bfc8492571d2f1699320ee19fc0746
Sonu64 <sonusantu64@gmail.com>
2026-01-01 12:52:17 +0530

    Added a new Folder for Divide and Conquer Problems, Added Working code for Merge Sort Algorithm.

--- Module16_Divide_And_Conquer/merge_sort.java
+++ Module16_Divide_And_Conquer/merge_sort.java
@@ -22,6 +22,7 @@ public class merge_sort {
         int j = mid+1; // Right iterator
         int k = 0; // Temp Array iterator
 
+        // Loop to put elements from both arrays into temp, Sorted !
         while (i <= mid && j <= end) {
             // Left element smaller
             if (arr[i] < arr[j]) {
@@ -36,6 +37,7 @@ public class merge_sort {
             k++;
         }
 
+        // But when the loop will finish, some elements will be remaining in one of the Parts
         // Elements left in Left array
         while (i <= mid) {
             temp[k] = arr[i];
@@ -46,17 +48,25 @@ public class merge_sort {
             temp[k] = arr[j];
             j++; k++;
         }
-
-        for (int f = 0, g = start; f<temp.length; f++, g++) {
-            arr[g] = temp[f];
-            g++;
+        // Copy Elements of temp to arr.
+        for (int tempIterator = 0, copyingIterator = start; tempIterator<temp.length; tempIterator++, copyingIterator++) {
+            arr[copyingIterator] = temp[tempIterator];
         }
     }
 
     public static void main(String[] args) {
         int[] arr = {34, 21, 56, 40, 90, 89, 91};
+
+        // Displaying original unsorted array
+        System.out.print("Original Array:\t");
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i] + "\t");
+
+        // Performing merge sort algorithm on the array, passed by referenc
         mergeSort(arr, 0, arr.length-1);
-        System.out.println("Sorted Array - \n");
+
+        // Displaying Sorted array
+        System.out.print("\nSorted Array:\t");
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + "\t");
         }

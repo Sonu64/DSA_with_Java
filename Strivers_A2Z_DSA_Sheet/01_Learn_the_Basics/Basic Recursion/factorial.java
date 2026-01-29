95d7bfe10919c5e3230aa510f0e81d451737f4be
Sonu64 <sonusantu64@gmail.com>
2026-01-29 16:02:15 +0530

    Added recursive method to calculate factorial of a number in new factorial.java file - Striver Folder

---
+++ Strivers_A2Z_DSA_Sheet/01_Learn_the_Basics/Basic Recursion/factorial.java
@@ -0,0 +1,12 @@
+// https://www.geeksforgeeks.org/problems/factorial5739/1
+
+class Solution {
+    // Function to calculate factorial of a number.
+    int factorial(int n) {
+        // code here
+        if (n==0)
+            return 1;
+        else 
+            return n * factorial(n-1);
+    }
+}

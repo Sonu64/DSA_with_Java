8d5e8a77840a49a20b9e5a70eb6eb5ba70cb5c43
Sonu64 <sonusantu64@gmail.com>
2026-02-10 12:08:00 +0530

    Add subsets generation implementation in subsets_LEETCODE.java. Wiped previous files.

---
+++ Module18_Backtracking/subsets_LEETCODE.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class subsets_LEETCODE {
+    // Global and Static: Accessible everywhere in this class
+    static List<List<Integer>> results = new ArrayList<>();
+
+    public static void solve(int[] nums, int i, List<Integer> temp) {
+        // Base Case
+        if (i == nums.length) {
+            results.add(new ArrayList<>(temp));
+            return;
+        }
+
+        // Choice 1: Include nums[i]
+        temp.add(nums[i]);
+        solve(nums, i + 1, temp);
+
+        // Backtrack: Remove the element to explore Choice 2
+        temp.remove(temp.size() - 1);
+
+        // Choice 2: Exclude nums[i]
+        solve(nums, i + 1, temp);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = in.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        // Start the recursion
+        solve(nums, 0, new ArrayList<>());
+
+        // Print the global results list
+        System.out.println("Generated Subsets: " + results);
+        
+        in.close();
+    }
+}

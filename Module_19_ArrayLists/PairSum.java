a30d962a1c96cebf1972bdeeb317d42ffb1b82b9
Sonu64 <sonusantu64@gmail.com>
2026-03-03 16:19:54 +0530

    Implement Two-Pointer Approach in PairSum class and update output formatting.

--- Module_19_ArrayLists/PairSum.java
+++ Module_19_ArrayLists/PairSum.java
@@ -17,6 +17,35 @@ public class PairSum {
         return pair;
     }
 
+    // Two-Pointer Approach
+    static ArrayList<Integer> solveUsing2Pointer(List<Integer> nums, int targetSum) {
+        int leftPointer = 0;
+        int rightPointer = nums.size()-1;
+        // At some point we must return the array
+        // containing left and right pointers.
+        while(leftPointer <= rightPointer) {
+            int sum = nums.get(leftPointer) + nums.get(rightPointer);
+            if (sum == targetSum) {
+                 // Found => Return 1-based indices
+                ArrayList<Integer> answer = new ArrayList<>();
+                answer.add(leftPointer);
+                answer.add(rightPointer);
+                return answer;
+            }  
+            else if (sum < targetSum) {
+                leftPointer++;
+            }
+            else {
+                rightPointer--;
+            }
+        }
+        // No solution found !
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(-1);
+        answer.add(-1);
+        return answer;
+    }
+
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list.add(1);
@@ -29,7 +58,10 @@ public class PairSum {
         int targetSum = 10;
 
         ArrayList<Integer> ans = solve(list, targetSum);
+        ArrayList<Integer> ans2 = solve(list, targetSum);
         
-        System.out.println(ans);
+        System.out.println("Result using Brute Force Approach = " + ans);
+        System.out.println("Result using 2-Pointer Approach = " + ans2);
+        System.out.println("P.S:- For this to work, the Array/ArrayList must be sorted !");
     }
 }

cee0b21ea591f9218670a8a465640ef6e75e1fba
Sonu64 <sonusantu64@gmail.com>
2026-03-07 13:54:33 +0530

    Change access modifiers for LinkedList methods and variables to public for better accessibility; improve code readability.

--- Module_20_LinkedLists/LinkedList.java
+++ Module_20_LinkedLists/LinkedList.java
@@ -12,45 +12,49 @@ public class LinkedList {
     }
 
     // Class level, needs objects to be made to access these. Non-static.
-    public Node head; // may put static for both head and tail, but would be unable to create multiple lists then.
-    public Node tail;
+    private Node head; // may put static for both this.head and this.tail, but would be unable to create multiple lists then.
+    private Node tail;
     
-    private void addFirst(int data) {
+    public void addFirst(int data) {
         //Step-1: Create the new Node
         Node temp = new Node(data);
         //..If Linked List is empty..
-        if(head == null) {
-            head = tail = temp;
+        if(this.head == null) {
+            this.head = this.tail = temp;
             return;
         }
         else {
-            // Swap between head and the new first node made
-            temp.next = head;
-            head = temp;
+            // Swap between this.head and the new first node made
+            temp.next = this.head;
+            this.head = temp;
         }
     }
     
-    private void addLast(int data) {
+    public void addLast(int data) {
         Node temp = new Node(data);
         // Linked List is empty 
-        if (head == null) {
-            head = tail = temp;
+        if (this.head == null) {
+            this.head = this.tail = temp;
             return;
         }
         //Else
         else {
-            tail.next = temp;
-            tail = temp;
+            this.tail.next = temp;
+            this.tail = temp;
         }
     }
 
-    private void printList() {
-        Node curr = head;
-        // This is beautiful condition checking! Checking curr.next will not work, same for checking curr != tail..Just check when curr becomes null itself after moving to its "next" part.
+    public void printList() {
+        Node curr = this.head;
+        // This is beautiful condition checking! Checking curr.next will not work, same for checking curr != this.tail..Just check when curr becomes null itself after moving to its "next" part.
         while (curr != null) {
             System.out.println(curr.data + "-->");
             curr = curr.next;
         }
+    }
+
+    public void addAtIndex(int index, int data) {
+        
     }
 
     public static void main(String[] args) {
@@ -59,7 +63,7 @@ public class LinkedList {
         
         // A new instance of LinkedList class, this is a LinkedList Object. Made inside the static main method.
 
-        // A Static method (like main) is like a ghost—it has no "body" (object). Therefore, it cannot see "Instance" variables like head or tail directly because it doesn't know which house's door you're talking about. That's why we "have to" create our instance-variables -> Objects of the class.
+        // A Static method (like main) is like a ghost—it has no "body" (object). Therefore, it cannot see "Instance" variables like this.head or this.tail directly because it doesn't know which house's door you're talking about. That's why we "have to" create our instance-variables -> Objects of the class.
 
         // By making main static, Java says: "I will let you run this code globally so you can 'spawn' the first objects into existence."
 

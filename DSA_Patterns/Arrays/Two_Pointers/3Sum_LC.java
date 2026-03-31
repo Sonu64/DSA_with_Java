2435a326e90b3f7cbe15bd754759e5763c5fad71
Sonu64 <sonusantu64@gmail.com>
2026-03-31 19:51:24 +0530

    Solved 3Sum in the Best-way possible, no HashSet, understood External and Internal Duplicate skipping.

--- DSA_Patterns/Arrays/Two_Pointers/3Sum_LC.java
+++ DSA_Patterns/Arrays/Two_Pointers/3Sum_LC.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -7,32 +9,32 @@ class Solution {
 
         List<List<Integer>> answer = new ArrayList<>();
 
-        for (int anchor = 0; anchor <= nums.length - 3; anchor++) { // Corrected: Needs room for 3 elements
-
-            int leftPointer = anchor + 1;
-            int rightPointer = nums.length - 1;
-            
-            while (leftPointer < rightPointer) { // Corrected: < to ensure 3 distinct indices
-                int sum = nums[anchor] + nums[leftPointer] + nums[rightPointer];
-                
+        int anchor = 0;
+        for(anchor = 0; anchor <= nums.length-3; anchor++) {
+            int left = anchor + 1, right = nums.length-1;
+            // Skip Anchor Duplicates - If 2 back-to-back anchors come out as SAME !
+            if(anchor > 0 && nums[anchor] == nums[anchor-1])
+                continue;
+            while (left < right) {
+                int sum = nums[anchor] + nums[left] + nums[right];
                 if (sum < TARGET) 
-                    leftPointer++;
+                    left++;
                 else if (sum > TARGET) 
-                    rightPointer--;
+                    right--;
                 else {
-                    ArrayList<Integer> triplet = new ArrayList<>();
-                    triplet.add(nums[anchor]);
-                    triplet.add(nums[leftPointer]);
-                    triplet.add(nums[rightPointer]);
-                    answer.add(triplet);
-
-                    leftPointer++;
-                    rightPointer--;
+                    answer.add(Arrays.asList(nums[anchor], nums[left], nums[right]));
+                    
+                    // Skip Internal Duplicates, like - 1, 1, 1...but the 1st 1 was left OR right..we should skip the next all positions
+                    while(left < right && nums[left] == nums[left+1]) 
+                        left++;
+                    while(right > left && nums[right] == nums[right-1])
+                        right--;
+                    // Only increment the pointers here, once we are sure that incrementing them will result in Distinct Numbers
+                    left++;
+                    right--;
                 }
             }
         }
-        
-        Set<List<Integer>> answerSet = new HashSet<>(answer);
-        return new ArrayList<>(answerSet); // Corrected: ArrayList instead of List
+        return answer;
     }
 }

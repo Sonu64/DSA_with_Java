f0c34750188c31fc76786c79e003486ba8abd313
Sonu64 <sonusantu64@gmail.com>
2026-04-05 10:57:57 +0530

    Refactor longestMountain method for improved clarity and performance.

--- DSA_Patterns/Arrays/Two_Pointers/Longest_Mountain_in_Array.java
+++ DSA_Patterns/Arrays/Two_Pointers/Longest_Mountain_in_Array.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int longestMountain(int[] arr) {
-        if(!(arr.length >= 3)) {
-            return 0;
-        }
-        int leftBase = 0, peak = 0, rightBase = 0, i = 0, j = 0;
-        int height = 0, maxHeight = Integer.MIN_VALUE;
-        while (leftBase < arr.length - 2) {
-            if(arr[leftBase] < arr[leftBase+1])
-            i = leftBase;
-            while(i<arr.length-1 && arr[i] < arr[i+1]) {     
-                i++;
-            }
-            peak = i;
-            j = peak;
-            while(j<arr.length-1 && arr[j] > arr[j+1]) {                    
-                j++;
-            }
-            rightBase = j;
-            height = rightBase-leftBase+1;
-            if (height > maxHeight && peak<j) 
-                maxHeight = height;
-            leftBase++;
-        }
-        return maxHeight;
+  public int longestMountain(int[] arr) {
+    
+    if (!(arr.length >= 3)) {
+      return 0;
     }
+
+    int i = 1, peak = 0, maxLength = 0;
+    while (i < arr.length-1) {
+      if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+        peak = i;
+        int left = peak - 1;
+        int right = peak + 1;
+        // Boundary checks below are also important ! And these dont make complexity O(n^2), they are
+        // simply overwriting, not "processing" those elements back again !
+        while (left > 0 && arr[left - 1] < arr[left]) left--;
+        while (right < arr.length-1 && arr[right + 1] < arr[right]) right++;
+        int currentLength = right - left + 1;
+        maxLength = (currentLength > maxLength) ? currentLength : maxLength;
+      }
+      i++;
+    }
+    return maxLength;
+  }
 }
